dddcc3c6edfe27e28b505bce78bfb02997908679
Ethnolatin <duchesne.fred@gmail.com>
2020-11-04 16:56:18 +0100

    affiche le produit (hors options) et le bouton "Add to cart" sur la page produit

--- js/product.js
+++ js/product.js
@@ -2,10 +2,14 @@ const params = new URLSearchParams(document.location.search);
 const id = params.get("id");
 const request = "http://localhost:3000/api/furniture/"+id;
 const productImage = document.getElementsByTagName("img")[0];
+const productName = document.getElementById("name");
+const productPrice = document.getElementById("price");
+const productDescription = document.getElementById("description");
 
 ajaxGet(request, function (response) {
   const productElt = JSON.parse(response);
   productImage.src = productElt.imageUrl;
-  console.log(productImage.src);
-
+  productName.textContent = productElt.name;
+  productPrice.textContent = "€ "+(productElt.price/100).toFixed(2);
+  productDescription.textContent = productElt.description;
 });
