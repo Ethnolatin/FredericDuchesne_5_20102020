9759f31f99d214f4d6d6cbf114b695ccb054ea74
Ethnolatin <duchesne.fred@gmail.com>
2020-11-04 13:30:53 +0100

    affiche la photo du produit sur la page produit

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
-var listElt = document.getElementById("furnitures");
+const listElt = document.getElementById("furnitures");
 
 ajaxGet("http://localhost:3000/api/furniture", function (response) {
     // Transforme la réponse en tableau d'objets JavaScript
-    var furnitures = JSON.parse(response);
+    const furnitures = JSON.parse(response);
     // Affiche le nom de chaque meuble
     furnitures.forEach(function (furniture) {
-        var furnitureElt = document.createElement("li");
-        var linkAddress = "./html/product.html?id="+furniture._id;
-        var linkElt = document.createElement("a");
+        const furnitureElt = document.createElement("li");
+        const linkAddress = "./html/product.html?id="+furniture._id;
+        const linkElt = document.createElement("a");
         linkElt.textContent = furniture.name;
         linkElt.href = linkAddress;
         furnitureElt.appendChild(linkElt);
         listElt.appendChild(furnitureElt);
-        console.log()
     })
 });
--- js/product.js
+++ js/product.js
@@ -1,3 +1,11 @@
-let params = new URLSearchParams(document.location.search);
-let id = params.get("id");
-console.log(id)
+const params = new URLSearchParams(document.location.search);
+const id = params.get("id");
+const request = "http://localhost:3000/api/furniture/"+id;
+const productImage = document.getElementsByTagName("img")[0];
+
+ajaxGet(request, function (response) {
+  const productElt = JSON.parse(response);
+  productImage.src = productElt.imageUrl;
+  console.log(productImage.src);
+
+});
