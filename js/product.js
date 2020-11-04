c22aca1c836140b8bedd6cc4bb721e68b4bd04e7
Ethnolatin <duchesne.fred@gmail.com>
2020-11-04 18:10:57 +0100

    ajoute le menu de sélection du vernis à la page produit

--- js/product.js
+++ js/product.js
@@ -5,6 +5,7 @@ const productImage = document.getElementsByTagName("img")[0];
 const productName = document.getElementById("name");
 const productPrice = document.getElementById("price");
 const productDescription = document.getElementById("description");
+const varnish = document.getElementById("varnish");
 
 ajaxGet(request, function (response) {
   const productElt = JSON.parse(response);
@@ -12,4 +13,14 @@ ajaxGet(request, function (response) {
   productName.textContent = productElt.name;
   productPrice.textContent = "€ "+(productElt.price/100).toFixed(2);
   productDescription.textContent = productElt.description;
+  const varnishes = productElt.varnish;
+  varnishes.forEach(function (varnishOption) {
+    console.log(varnishOption)
+    const varnishElt = document.createElement("option");
+    varnishElt.setAttribute("value", varnishOption);
+    varnishElt.textContent = varnishOption;
+    varnish.appendChild(varnishElt);
+})
+
+
 });
