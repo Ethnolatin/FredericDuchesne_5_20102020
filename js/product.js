7b4bc1660f2071dac8def8a96bea3f9c987516e2
Ethnolatin <duchesne.fred@gmail.com>
2020-11-05 01:09:55 +0100

    met en place le panier dans le local storage et y ajoute les produits au click sur le bouton

--- js/product.js
+++ js/product.js
@@ -1,11 +1,13 @@
-const params = new URLSearchParams(document.location.search);
-const id = params.get("id");
+const param = window.location.search;
+const id = param.substring(4);
 const request = "http://localhost:3000/api/furniture/"+id;
 const productImage = document.getElementsByTagName("img")[0];
 const productName = document.getElementById("name");
 const productPrice = document.getElementById("price");
 const productDescription = document.getElementById("description");
 const varnish = document.getElementById("varnish");
+const addToCartBtn = document.getElementById("addToCart");
+var cartContent = JSON.parse(localStorage.getItem("cart")) || [];
 
 ajaxGet(request, function (response) {
   const productElt = JSON.parse(response);
@@ -15,12 +17,23 @@ ajaxGet(request, function (response) {
   productDescription.textContent = productElt.description;
   const varnishes = productElt.varnish;
   varnishes.forEach(function (varnishOption) {
-    console.log(varnishOption)
     const varnishElt = document.createElement("option");
     varnishElt.setAttribute("value", varnishOption);
     varnishElt.textContent = varnishOption;
     varnish.appendChild(varnishElt);
-})
+  })
 
+  addToCartBtn.addEventListener("click", function(){
+    const addedProduct = {
+      id: id,
+      image: productElt.imageUrl,
+      name: productElt.name,
+      price: productElt.price/100}
+    cartContent.push(addedProduct);
+    localStorage.setItem("cart", JSON.stringify(cartContent));
+    alert("Product added to cart");
+  })
 
 });
+
+
