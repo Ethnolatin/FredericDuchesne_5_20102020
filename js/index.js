f7f6ee448b9bbf3b45670bdd469e4af0d2a07374
Ethnolatin <duchesne.fred@gmail.com>
2020-11-07 00:51:35 +0100

    améliore la vue de la liste en première page

--- js/index.js
+++ js/index.js
@@ -2,14 +2,20 @@ const listElt = document.getElementById("furnitures");
 
 ajaxGet("http://localhost:3000/api/furniture")
 	.then(function (furnitures) {
-		// Affiche le nom de chaque meuble
+		// Affiche le nom de chaque meuble dans une liste
 		furnitures.forEach(function (furniture) {
-			const furnitureElt = document.createElement("li");
+			const furnitureElt = document.createElement("a");
+			const furnitureImage = document.createElement("img");
+			const furnitureName = document.createElement("span")
 			const linkAddress = "./html/product.html?id="+furniture._id;
-			const linkElt = document.createElement("a");
-			linkElt.textContent = furniture.name;
-			linkElt.href = linkAddress;
-			furnitureElt.appendChild(linkElt);
+			furnitureName.textContent = furniture.name;
+			furnitureName.className = "h5 pl-2"
+			furnitureImage.src = furniture.imageUrl;
+			furnitureImage.width = 50;
+			furnitureElt.href = linkAddress;
+			furnitureElt.className = "list-group-item list-group-item-action";
+			furnitureElt.appendChild(furnitureImage);
+			furnitureElt.appendChild(furnitureName);
 			listElt.appendChild(furnitureElt);
 		});
 	})
